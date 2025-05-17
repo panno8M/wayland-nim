08d70d2faea08750b6d64b2ddadba3505ca19287
la .panon. <panno.devel@gmail.com>
2025-05-18 00:09:30 +0900

    Generate

--- src/wayland/native/client.nim
+++ src/wayland/native/client.nim
@@ -1,3 +1,3 @@
 import client_core; export client_core
-import client_protocol; export client_protocol
+import wayland/protocols/wayland/client as client_protocol; export client_protocol
 import common; export common
--- src/wayland/native/server.nim
+++ src/wayland/native/server.nim
@@ -1,3 +1,3 @@
 import server_core; export server_core
-import server_protocol; export server_protocol
+import wayland/protocols/wayland/server as server_protocol; export server_protocol
 import common; export common
